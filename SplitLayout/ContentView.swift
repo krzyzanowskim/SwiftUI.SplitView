a4ccede8634f4cf959ccf652bd09ba1db381a647
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2022-07-23 15:15:02 +0200

    Initial Commit

---
+++ SplitLayout/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SplitLayout
+//
+//  Created by Marcin Krzyzanowski on 23/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitLayout/SplitLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitLayoutApp.swift
+//  SplitLayout
+//
+//  Created by Marcin Krzyzanowski on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SplitLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
